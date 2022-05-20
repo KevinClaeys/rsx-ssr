ee91e112906eff32c0eaef95fd6c24f38e70960f
Kevin Claeys <kvnclaeys@gmail.com>
2022-05-20 20:30:25 +0200

    base href rewrite

--- scully.rsx-ssr.config.ts
+++ scully.rsx-ssr.config.ts
@@ -1,6 +1,13 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+
+const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite]
+setPluginConfig(baseHrefRewrite, {
+  href: '/rsx-ssr',
+})
 
 export const config: ScullyConfig = {
+  defaultPostRenderers,
   projectRoot: "./src",
   projectName: "rsx-ssr",
   // add spsModulePath when using de Scully Platform Server,
